5b39f5ed99bea021149abd5f35ee1ecbea5c6eb4
Liam T O'Toole <lotoole@uvm.edu>
2018-11-13 10:49:58 -0500

    finished styles of footer, active menu item as well

--- wp-content/themes/winterfest/footer.php
+++ wp-content/themes/winterfest/footer.php
@@ -2,10 +2,15 @@
   <div class="container">
     <div class="row">
       <div class="col-md-4">
-        
+        <div class="content">
+          <span>©Waterbury Winter Fest.</span>
+          <span>All rights reserved</span>
+        </div>
       </div>
       <div class="col-md-8">
-
+        <div class="nav-wrap">
+          <?php wp_nav_menu( array( 'theme_location' => 'footer', 'container' => false ) ); ?>
+        </div>
       </div>
     </div>
   </div>
