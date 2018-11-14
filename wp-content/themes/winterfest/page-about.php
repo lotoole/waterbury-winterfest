e737269777d7750babc3a9285d129438f10a625d
Liam T O'Toole <lotoole@uvm.edu>
2018-11-14 13:09:57 -0500

    contact section front endand back end complete

--- wp-content/themes/winterfest/page-about.php
+++ wp-content/themes/winterfest/page-about.php
@@ -14,7 +14,7 @@ get_header();
 ?>
 <section class="mission">
   <div class="container">
-    <div class="col-md-8">
+    <div class="col-md-6">
       <div class="grid">
         <div class="grid-sizer"></div>
         <div class="grid-item">
@@ -34,8 +34,68 @@ get_header();
   </div>
 </section>
 
-<section>
-
+<section class="contact">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1><?php the_field('title'); ?></h1>
+      </div>
+      <div class="col-md-6">
+        <p class="intro"><?php the_field('interest_promo'); ?></p>
+        <ul>
+          <?php if( have_rows('list') ): $count=1; while( have_rows('list') ) : the_row(); ?>
+            <li><?php the_sub_field('interest'); ?></li>
+          <?php endwhile; endif; ?>
+        </ul>
+        <?php $image = get_field('contact_image'); $size = 'square_four'; ?>
+        <?php echo wp_get_attachment_image( $image, $size ); ?>
+      </div>
+      <div class="col-md-6">
+        <form>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="firstName">First Name</label>
+              <input type="text" class="form-control" id="firstName" placeholder="First Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="lastName">Last Name</label>
+              <input type="text" class="form-control" id="lastName" placeholder="Last Name">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <div class="">
+              <label for="purpose">Purpose</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+              <label class="form-check-label" for="inlineCheckbox1">Sponsorship</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+              <label class="form-check-label" for="inlineCheckbox2">Tickets/Registration</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+              <label class="form-check-label" for="inlineCheckbox2">Schedule</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+              <label class="form-check-label" for="inlineCheckbox2">Other</label>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="message">Message</label>
+            <textarea class="form-control" id="message" rows="8"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </section>
 
 <?php get_footer(); ?>
