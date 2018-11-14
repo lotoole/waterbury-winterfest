f4f13e9a5fd75d17bbbf465ca7bdc9da00d4813b
Liam T O'Toole <lotoole@uvm.edu>
2018-11-13 21:04:40 -0500

    register promo section front end done

--- wp-content/themes/winterfest/page-registration.php
+++ wp-content/themes/winterfest/page-registration.php
@@ -5,9 +5,52 @@ the_post();
 get_header();
 ?>
 
-<section class="hero">
-  <h1>Registration Page</h1>
+<?php
+  if ( have_rows( 'flexible_content' ) ) {
+      while ( have_rows( 'flexible_content' ) ) {
+          the_row();
+          get_template_part( 'partials/' . get_row_layout() );
+      }
+  }
+?>
+<section class="registration-promo">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2>Registration</h2>
+        <p class="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+      </div>
+      <div class="col-md-4">
+        <div class="content">
+          <h4>Broomball</h4>
+          <span>$80/team</span>
+          <span>Saturday</span>
+          <span>Jan 27,10am</span>
+          <a href="#" class="btn btn-primary">Register</a>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="content">
+          <h4>Broomball</h4>
+          <span>$80/team</span>
+          <span>Saturday</span>
+          <span>Jan 27,10am</span>
+          <a href="#" class="btn btn-primary">Register</a>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="content">
+          <h4>Broomball</h4>
+          <span>$80/team</span>
+          <span>Saturday</span>
+          <span>Jan 27,10am</span>
+          <a href="#" class="btn btn-primary">Register</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
+
 
 
 <?php get_footer(); ?>
--- wp-content/themes/winterfest/page-schedule.php
+++ wp-content/themes/winterfest/page-schedule.php
@@ -190,5 +190,96 @@ get_header();
   </div>
 </section>
 
+<section class="days">
+  <div class="row">
+    <!-- start of day -->
+    <div class="col-md-6">
+      <div class="hero">
+        <h1>Day One</h1>
+        <span>1/3/19</span>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="content-wrap">
+        <h2>Saturday, January 3</h2>
+        <div class="content">
+          <div class="event">
+            <h5>Opening Ceremony blah blah</h5>
+            <ul>
+              <li>5:30pm</li>
+              <li>GMCR Cafe Start</li>
+            </ul>
+            <a href="#" class="btn btn-primary">More</a>
+          </div>
+          <div class="event">
+            <h5>Opening Ceremony blah blah</h5>
+            <ul>
+              <li>5:30pm</li>
+              <li>GMCR Cafe Start</li>
+            </ul>
+            <a href="#" class="btn btn-primary">More</a>
+          </div>
+          <div class="event">
+            <h5>Opening Ceremony blah blah</h5>
+            <ul>
+              <li>5:30pm</li>
+              <li>GMCR Cafe Start</li>
+            </ul>
+            <a href="#" class="btn btn-primary">More</a>
+          </div>
+          <div class="event">
+            <h5>Opening Ceremony blah blah</h5>
+            <ul>
+              <li>5:30pm</li>
+              <li>GMCR Cafe Start</li>
+            </ul>
+            <a href="#" class="btn btn-primary">More</a>
+          </div>
+          <div class="event">
+            <h5>Opening Ceremony blah blah</h5>
+            <ul>
+              <li>5:30pm</li>
+              <li>GMCR Cafe Start</li>
+            </ul>
+            <a href="#" class="btn btn-primary">More</a>
+          </div>
+          <div class="event">
+            <h5>Opening Ceremony blah blah</h5>
+            <ul>
+              <li>5:30pm</li>
+              <li>GMCR Cafe Start</li>
+            </ul>
+            <a href="#" class="btn btn-primary">More</a>
+          </div>
+          <div class="event">
+            <h5>Opening Ceremony blah blah</h5>
+            <ul>
+              <li>5:30pm</li>
+              <li>GMCR Cafe Start</li>
+            </ul>
+            <a href="#" class="btn btn-primary">More</a>
+          </div>
+          <div class="event">
+            <h5>Opening Ceremony blah blah</h5>
+            <ul>
+              <li>5:30pm</li>
+              <li>GMCR Cafe Start</li>
+            </ul>
+            <a href="#" class="btn btn-primary">More</a>
+          </div>
+          <div class="event">
+            <h5>Opening Ceremony blah blah</h5>
+            <ul>
+              <li>5:30pm</li>
+              <li>GMCR Cafe Start</li>
+            </ul>
+            <a href="#" class="btn btn-primary">More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 
 <?php get_footer(); ?>
