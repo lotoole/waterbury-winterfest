e8926b72135819214d37aadad9be2690e9859681
Liam T O'Toole <lotoole@uvm.edu>
2018-11-14 21:39:15 -0500

    finished grant section stlyes

--- wp-content/themes/winterfest/page-about.php
+++ wp-content/themes/winterfest/page-about.php
@@ -21,13 +21,13 @@ get_header();
           <img src="https://fillmurray.com/300/300" alt="">
         </div>
         <div class="grid-item grid-item--width2">
-          <img src="https://fillmurray.com/500/300" alt="">
+          <img src="https://fillmurray.com/300/300" alt="">
         </div>
         <div class="grid-item">
           <img src="https://fillmurray.com/300/300" alt="">
         </div>
         <div class="grid-item grid-item--width2">
-          <img src="https://fillmurray.com/500/300" alt="">
+          <img src="https://fillmurray.com/300/300" alt="">
         </div>
       </div>
     </div>
@@ -60,27 +60,36 @@ get_header();
 <section class="grants">
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-12 numero-uno">
         <div class="intro-wrap">
-          <h1>Grants</h1>
-          <p class="intro">While Winterfest has always put its proceeds towards enhancing recreation in town, 2018 was the first year that grants were awarded for recreation-related programs and projects. Learn more about the grant criteria by checking out the applications at [link]. Keep in mind that the due date is ____ . Winterfest was thrilled to support such a wide range of recreational activity last year. Take a look at our grant recipients.</p>
+          <h1><?php the_field('grant_title'); ?></h1>
+          <p class="intro"><?php the_field('grant_intro'); ?></p>
         </div>
       </div>
-      <div class="col-md-6">
-        <img src="https://fillmurray.com/300/100" alt="">
-      </div>
+      <?php if( have_rows('grant') ): $count=1; while( have_rows('grant') ) : the_row(); ?>
+        <?php if($count === 1): ?>
+          <div class="col-md-6 beg-grant">
+            <img src="https://fillmurray.com/300/100" alt="">
+          </div>
+        <?php else: ?>
+          <div class="col-md-6 beg-grant">
+            <img src="https://fillmurray.com/300/100" alt="">
+          </div>
+        <?php endif; ?>
       <div class="col-md-6">
         <div class="name-wrap">
-          <h3>Company Name</h3>          
+          <h3><?php the_sub_field('company_name'); ?></h3>
         </div>
       </div>
       <div class="col-md-12">
-        <h3 class="award">Awarded:<span>$500</span></h3>
+        <h3 class="award">Awarded:<span>$<?php the_sub_field('award_amount'); ?></span></h3>
       </div>
-      <div class="col-md-12">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
-        <a href="#" class="btn btn-primary">More</a>
+      <div class="col-md-12 end-grant">
+        <p><?php the_sub_field('company_intro'); ?></p>
+        <a href="<?php the_sub_field('company_linnk'); ?>" class="btn btn-primary">More</a>
       </div>
+        <?php $count++; ?>
+      <?php endwhile; endif; ?>
     </div>
   </div>
 </section>
